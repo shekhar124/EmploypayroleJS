2956db94624ecd2ae0b61e5f7358d10bb9f71ba6
rajashekhar <rajashekharpasikanti@gmail.com>
2021-10-09 21:19:51 +0530

    Calculate daily employee wage on FT or PT

--- employ.js
+++ employ.js
@@ -1,6 +1,19 @@
-let isPresent = 1;
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == isPresent)
-    console.log("Employee is Present");
-else
-    console.log("Employee is Absent");
+let EMP_RATE_PER_HOUR = 20;
+let IS_PART_TIME = 1;
+let IS_FULL_TIME = 2;
+let empHrs=0;
+let empCheck = Math.round(Math.random() * 10) % 3;
+switch(empCheck)
+{
+    case IS_PART_TIME:
+        empHrs = 4;
+        break
+    case IS_FULL_TIME:
+        empHrs = 8;
+        break
+    default:
+        empHrs = 0;
+}
+
+empWage = empHrs * EMP_RATE_PER_HOUR;
+console.log("Employee Wage = "+empWage);
